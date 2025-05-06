09343a1d3b01a1b9a58f25dea677ac92c962d563
Luiz Felipe <solelydev1@gmail.com>
2025-05-06 11:39:09 -0300

    feat: creating workoutRequest/workoutResponse DTOs

---
+++ src/main/java/br/com/trainingapi/workoutplanner/dto/WorkoutResponse.java
@@ -0,0 +1,15 @@
+package br.com.trainingapi.workoutplanner.dto;
+
+public record WorkoutResponse(
+
+        Long id,
+
+        String title,
+
+        String description,
+
+        String trainingDate,
+
+        Long userId)
+
+{}
